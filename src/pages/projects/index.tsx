e1c95724fbcde92cb646c513006e41f0a8f0b2b3
ken <racelisjk@gmail.com>
2022-09-25 23:29:21 +0800

    modified the pdf file

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -1,5 +1,5 @@
 import { useRef, useEffect, useState } from "react";
-import {motion } from 'framer-motion'
+import { motion } from 'framer-motion'
 import Card from "../../components/card/index";
 import { useIntersection, useMediaQuery } from "@mantine/hooks";
 import { useAtom } from "jotai";
@@ -10,31 +10,31 @@ export default function Profile() {
   const DATA = [
     {
       title: "Portfolio",
-      disc: "My portfolio - React",
-      language: "Typescript",
+      disc: "React - My portfolio",
+      language: "Typescript, Tailwind, Jotai, Framer motion",
       id: 1,
       url: "https://github.com/K3ndev/Portfolio",
     },
     {
       title: "search-anime",
-      disc: "A react application that can search an anime, that use jikan API.",
-      language: "Typescript",
+      disc: "React - Am application that can search an anime, that use jikan API.",
+      language: "Typescript, Tailwind, ReactQuery",
       id: 2,
       url: "https://search-anime.vercel.app/",
     },
     {
       title: "hotel-reservation",
-      disc: "A step by step form reservation that build in ReactJs  ",
-      language: "Typescript",
+      disc: "React - A step by step form reservation",
+      language: "Typescript, Mantine",
       id: 3,
       url: "https://k3n-hotel-reservation.vercel.app/",
     },
     {
-      title: "Proj 4",
-      disc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit Lorem ipsum dolor sit amet, consectetur adipiscing elit Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-      language: "Typescript",
+      title: "book-store",
+      disc: "Next js - book store",
+      language: "Typescript, Tailwind, Zustand",
       id: 4,
-      url: "",
+      url: "https://k3n-book-store.vercel.app/",
     },
     {
       title: "Proj 5",
@@ -53,7 +53,7 @@ export default function Profile() {
   ];
 
   // animation state 
-  const [opacityIncrease, setOpacityIncrease] =  useState(0)
+  const [opacityIncrease, setOpacityIncrease] = useState(0)
   const matches = useMediaQuery('((min-width: 768px)');
 
   // listener for div is visible or not
@@ -77,30 +77,30 @@ export default function Profile() {
       });
       setOpacityIncrease(1)
     }
-    if(isIntersecting === false && matches){
+    if (isIntersecting === false && matches) {
       setOpacityIncrease(0)
     }
   }, [isIntersecting]);
 
   return (
-      <motion.section
-      initial={{ opacity: 0}} animate={{ opacity: opacityIncrease }} transition={{ duration: 0.5}}
+    <motion.section
+      initial={{ opacity: 0 }} animate={{ opacity: opacityIncrease }} transition={{ duration: 0.5 }}
       className="px-10 mb-72 md:w-3/4 md:m-auto lg:w-2/3 md:h-screen md:snap-start md:flex md:items-center"
       id="Projects"
-      >
-        <div >
-          <div>
-            <h1 className="text-trinary mb-3 prose prose-xl md:prose-2xl font-Raleway md:mb-1" ref={ref}>
-              Projects.
-            </h1>
-          </div>
-
-          <div className="grid gap-6 md:gap-1 lg:gap-3 grid-cols-1 mt-10 md:mt-0 md:grid-cols-2 lg:grid-cols-3" >
-            {DATA.map((item) => (
-              <Card key={item.id} proj={item} />
-            ))}
-          </div>
+    >
+      <div >
+        <div>
+          <h1 className="text-trinary mb-3 prose prose-xl md:prose-2xl font-Raleway md:mb-1" ref={ref}>
+            Projects.
+          </h1>
         </div>
-      </motion.section>
+
+        <div className="grid gap-6 md:gap-1 lg:gap-3 grid-cols-1 mt-10 md:mt-0 md:grid-cols-2 lg:grid-cols-3" >
+          {DATA.map((item) => (
+            <Card key={item.id} proj={item} />
+          ))}
+        </div>
+      </div>
+    </motion.section>
   );
 }
